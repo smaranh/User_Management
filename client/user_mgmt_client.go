04a05dcff7f2373774f46c4f514c796b1853cd8f
Smaran Harihar <smaran.harihar@gmail.com>
2021-10-16 09:37:15 -0700

    add user management client

---
+++ client/user_mgmt_client.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "example.com/go-usermgmt-grpc/proto"
+	"google.golang.org/grpc"
+)
+
+const (
+	address = "localhost:5051"
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("Did not connect %v", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewUserManagementClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	var new_users = make(map[string]int32)
+	new_users["Janice"] = 35
+	new_users["Chandler"] = 33
+
+	for name, age := range new_users {
+		r, err := c.CreateNewUser(ctx, &pb.NewUser{
+			Name: name,
+			Age:  age,
+		})
+		if err != nil {
+			log.Fatalf("could not create new user: %v", err)
+		}
+
+		log.Printf(`User Details: 
+		Name: %s,
+		Age: %d,
+		Id: %d
+		`, r.GetName(), r.GetAge(), r.GetId())
+	}
+}
