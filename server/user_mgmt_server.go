990f39c4588b16355f26d26332f6a2d34567a70e
Smaran Harihar <smaran.harihar@gmail.com>
2021-10-16 09:37:05 -0700

    add user management server

---
+++ server/user_mgmt_server.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"log"
+	"math/rand"
+	"net"
+
+	pb "example.com/go-usermgmt-grpc/proto"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":5051"
+)
+
+type UserManagementServer struct {
+	pb.UnimplementedUserManagementServer
+}
+
+func (s *UserManagementServer) CreateNewUser(ctx context.Context, in *pb.NewUser) (*pb.User, error) {
+	log.Printf("Received: %v", in.GetName())
+	var user_id int32 = int32(rand.Intn(1000))
+	return &pb.User{
+		Name: in.GetName(),
+		Age:  in.GetAge(),
+		Id:   user_id,
+	}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("Unable to listen on %s. Error: %v", port, err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterUserManagementServer(s, &UserManagementServer{})
+	log.Printf("server listening on %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
